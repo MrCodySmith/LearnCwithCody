a352542c8451103c06b89cddc7f1ddd1397c23c4
CodyFromIT <smith.11595@osu.edu>
2017-06-10 22:58:18 -0400

    Hello World!
    That’s right, we started out this language like everyone should. With
    Hello World!

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+
+    // This sample program displays the output Hellow World!
+    /* This sample program displays the output Hello World! */
+    /* At this point
+    This is repetitive */
+    printf( "Hello World! \n" );
+    return 0 ;
+
+}
