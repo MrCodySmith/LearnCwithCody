5b07742105cdf5e4c0c7054a457df89b7a1a02b1
CodyFromIT <smith.11595@osu.edu>
2017-06-13 18:28:11 -0400

    Hello, World!
    New book, new hello world!

--- helloworld.c
+++ helloworld.c
@@ -2,12 +2,7 @@
 
 main()
 {
-
-    // This sample program displays the output Hellow World!
-    /* This sample program displays the output Hello World! */
-    /* At this point
-    This is repetitive */
-    printf( "Hello World! \n" );
-    return 0 ;
-
+    
+    printf("Hello, world!\n");
+    
 }
